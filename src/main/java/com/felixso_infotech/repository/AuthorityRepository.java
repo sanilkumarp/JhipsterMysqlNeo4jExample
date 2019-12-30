5552e5ead8fdc4e949c7e9c3a60d5ed18d9f0515
sanilkumar <sanilkumar.p@lxisoft.com>
2019-12-30 17:48:40 +0530

    Initial version of JhipsterMysqlNeo4jSample generated by JHipster-6.6.0

---
+++ src/main/java/com/felixso_infotech/repository/AuthorityRepository.java
@@ -0,0 +1,11 @@
+package com.felixso_infotech.repository;
+
+import com.felixso_infotech.domain.Authority;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+/**
+ * Spring Data JPA repository for the {@link Authority} entity.
+ */
+public interface AuthorityRepository extends JpaRepository<Authority, String> {
+}
